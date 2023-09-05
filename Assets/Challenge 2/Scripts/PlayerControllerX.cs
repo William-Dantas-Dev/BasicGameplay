adc742756f22fda760f7aea5b76352b7dd925d1c
William-Dantas-Dev <95831281+William-Dantas-Dev@users.noreply.github.com>
2023-09-05 14:46:17 -0300

    Challanger 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool intervalShoot;
+    private float intervalTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !intervalShoot)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            intervalShoot = true;
+            Invoke("IntervalShootTime", intervalTime);
+        }
+    }
+
+    private void IntervalShootTime()
+    {
+        intervalShoot = false;
+    }
+}
